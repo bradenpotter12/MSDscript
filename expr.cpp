c353e59a432500428da10a0849867a83c986ead5
bradenpotter <practicaldreamer24@gmail.com>
2021-02-02 00:05:58 -0700

    Resolved merge conflict

--- expr.cpp
+++ expr.cpp
@@ -37,11 +37,8 @@ bool Num::has_variable() {
 }
 
 Expr* Num::subst(std::string string, Expr *e) {
-<<<<<<< HEAD
-    return nullptr;
-=======
+
     return this;
->>>>>>> work2
 }
 
 // Add Constructor implementation
@@ -72,25 +69,10 @@ bool Add::has_variable() {
     return this->lhs->has_variable() || this->rhs->has_variable();
 }
 
-<<<<<<< HEAD
-Expr* Add::subst(std::string string, Expr *variable) {
-    
-    if (this->lhs->equals(new Variable(string)) && this->rhs->equals(new Variable(string))) {
-        return new Add(variable, variable);
-    }
-    else if (this->lhs->equals(new Variable(string))) {
-        return new Add(variable, rhs);
-    }
-    else if (this->rhs->equals(new Variable(string))) {
-        return new Add(lhs, variable);
-    }
-    
-    return nullptr;
-=======
 Expr* Add::subst(std::string string, Expr *exprSub) {
     
     return new Add(this->lhs->subst(string, exprSub), this->rhs->subst(string, exprSub));
->>>>>>> work2
+
 }
 
 // Mult Constructor implementation
@@ -120,25 +102,10 @@ bool Mult::has_variable() {
     return this->lhs->has_variable() || this->rhs->has_variable();
 }
 
-<<<<<<< HEAD
-Expr* Mult::subst(std::string string, Expr *variable) {
-    
-    if (this->lhs->equals(new Variable(string)) && this->rhs->equals(new Variable(string))) {
-        return new Mult(variable, variable);
-    }
-    else if (this->lhs->equals(new Variable(string))) {
-        return new Mult(variable, rhs);
-    }
-    else if (this->rhs->equals(new Variable(string))) {
-        return new Mult(lhs, variable);
-    }
-    
-    return nullptr;
-=======
 Expr* Mult::subst(std::string string, Expr *exprSub) {
     
     return new Mult(this->lhs->subst(string, exprSub), this->rhs->subst(string, exprSub));
->>>>>>> work2
+
 }
 
 // Variable Constructor implementation
@@ -170,15 +137,6 @@ bool Variable::has_variable() {
     return true;
 }
 
-<<<<<<< HEAD
-Expr* Variable::subst(std::string string, Expr *variable) {
-    
-    if (this->string == string) {
-        return variable;
-    }
-    
-    return nullptr;
-=======
 Expr* Variable::subst(std::string string, Expr *exprSub) {
     
     if (this->string == string) {
@@ -186,7 +144,7 @@ Expr* Variable::subst(std::string string, Expr *exprSub) {
     }
     
     return this;
->>>>>>> work2
+
 }
 
 TEST_CASE( "Interp" ) {
@@ -218,14 +176,10 @@ TEST_CASE( "has_variable" ) {
 
 TEST_CASE( "subst" ) {
     
-<<<<<<< HEAD
-    CHECK( (new Num(8))->subst("x", new Variable("y")) == nullptr);
-=======
     CHECK( (new Num(8))->subst("x", new Variable("y"))->equals(new Num(8)));
     
     CHECK( (new Variable("x"))->subst("x", new Variable("y"))
           ->equals(new Variable("y")));
->>>>>>> work2
     
     CHECK( (new Add(new Variable("x"), new Num(7)))
            ->subst("x", new Variable("y"))
@@ -234,33 +188,6 @@ TEST_CASE( "subst" ) {
     CHECK( (new Add(new Num(7), new Variable("x")))
            ->subst("x", new Variable("y"))
            ->equals(new Add(new Num(7), new Variable("y"))) );
-<<<<<<< HEAD
-    
-    CHECK( (new Add(new Variable("x"), new Variable("x")))
-           ->subst("x", new Variable("y"))
-           ->equals(new Add(new Variable("y"), new Variable("y"))) );
-    
-    CHECK( (new Add(new Num(4), new Num(7)))
-          ->subst("x", new Variable("y")) == nullptr );
-    
-    CHECK( (new Mult(new Variable("x"), new Num(7)))
-           ->subst("x", new Variable("y"))
-           ->equals(new Mult(new Variable("y"), new Num(7))) );
-    
-    CHECK( (new Mult(new Num(7), new Variable("x")))
-           ->subst("x", new Variable("y"))
-           ->equals(new Mult(new Num(7), new Variable("y"))) );
-    
-    CHECK( (new Mult(new Variable("x"), new Variable("x")))
-           ->subst("x", new Variable("y"))
-           ->equals(new Mult(new Variable("y"), new Variable("y"))) );
-    
-    CHECK( (new Mult(new Num(4), new Num(7)))
-          ->subst("x", new Variable("y")) == nullptr );
-    
-    CHECK( (new Variable("x"))->subst("x", new Variable("y"))
-          ->equals(new Variable("y")));
-=======
 
     CHECK( (new Add(new Variable("x"), new Variable("x")))
            ->subst("x", new Variable("y"))
@@ -293,7 +220,7 @@ TEST_CASE( "subst" ) {
 
     CHECK( (new Add(new Add(new Variable("x"), new Num(1)), new Num(2)))
           ->subst("x", new Num(7))->equals( (new Add(new Add(new Num(7), new Num(1)), new Num(2)))));
->>>>>>> work2
+
 }
 
 
